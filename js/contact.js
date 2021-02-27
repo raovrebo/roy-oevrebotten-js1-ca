4b01b64fbebd5f9d8ec400b890e546822b96cb43
Roy Are Øvrebotten <69842808+raovrebo@users.noreply.github.com>
2021-02-27 18:30:47 +0100

    Created contact.js. Updated contact.html, style.css and index.html

---
+++ js/contact.js
@@ -0,0 +1,57 @@
+const form = document.querySelector("#contactForm");
+
+const fname = document.querySelector("#fname");
+const fnameError = document.querySelector("#fnameError");
+
+const subject = document.querySelector("#subject");
+const subjectError = document.querySelector("#subjectError");
+
+const email = document.querySelector("#email");
+const emailError = document.querySelector("#emailError");
+
+const address = document.querySelector("#address");
+const addressError = document.querySelector("#addressError");
+
+function validateForm(event) {
+    event.preventDefault();
+
+    if (checkLength(fname.value, 0) === true) {
+        fnameError.style.display = "none";
+    } else {
+        fnameError.style.display = "block";
+    }
+
+    if (checkLength(subject.value, 9) === true) {
+        subjectError.style.display = "none";
+    } else {
+        subjectError.style.display = "block";
+    }
+
+    if (validateEmail(email.value) === true) {
+        emailError.style.display = "none";
+    } else {
+        emailError.style.display = "block";
+    }
+
+    if (checkLength(address.value, 24) === true) {
+        addressError.style.display = "none";
+    } else {
+        addressError.style.display = "block";
+    }
+}
+
+form.addEventListener("submit", validateForm);
+
+function checkLength(value, len) {
+    if (value.trim().length > len) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function validateEmail(email) {
+    const regEx = /\S+@\S+\.\S+/;
+    const patternMatches = regEx.test(email);
+    return patternMatches;
+}
